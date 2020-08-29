57aa3588b5904e7158bfee41dd70940783f83756
Mandy <chenjiafeng16@126.com>
2020-08-29 21:48:51 +0800

    [springboot][mandy] feature : perfect Employee model

--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,8 +1,25 @@
 package com.example.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
-    Integer id;
-    String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Employee(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Employee() {
+    }
 
     public String getName() {
         return name;
@@ -12,10 +29,6 @@ public class Employee {
         this.name = name;
     }
 
-    public Employee(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public Integer getId() {
         return id;
