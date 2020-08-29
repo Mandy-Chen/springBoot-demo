7122d6790fd08888a7cf2bc0f27a410224bc49ca
Mandy <chenjiafeng16@126.com>
2020-08-29 22:38:44 +0800

    [springboot][mandy] feature : Successfully connected to MySQL database and successfully added data

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -2,19 +2,27 @@ package com.example.controller;
 
 import com.example.model.Employee;
 import com.example.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    @Autowired
     EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.add(employee);
+    }
+
+    @GetMapping
+    public String getAllEmployee() {
+        System.out.println("getAllEmployee");
+        return "sss";
     }
 }
--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -2,11 +2,12 @@ package com.example.service;
 
 import com.example.model.Employee;
 import com.example.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-
+    @Autowired
     EmployeeRepository employeeRepository;
 
     public Employee add(Employee emplyee) {
