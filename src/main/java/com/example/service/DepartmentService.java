b77920da6c5812b0911f6a8c9a40ea8a88748160
Mandy <chenjiafeng16@126.com>
2020-08-30 17:49:10 +0800

    [springboot][mandy] feature : implement should_return_all_department_when_get_all_department_given_nothing

--- src/main/java/com/example/service/DepartmentService.java
+++ src/main/java/com/example/service/DepartmentService.java
@@ -1,14 +1,18 @@
 package com.example.service;
 
 import com.example.model.Department;
+import com.example.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class DepartmentService {
+    @Autowired
+    DepartmentRepository departmentRepository;
 
     public List<Department> getAllDepartments() {
-        return null;
+        return departmentRepository.findAll();
     }
 }
